c5a823cc258d8c2fd2bd4b6db64cb3b70c5c5441
Okan Hollander <okan.hollander@quality.nl>
2023-05-16 21:10:08 +0200

    Initial Commit

--- 06_Functions_Intro/03_palindromes.py
+++ 06_Functions_Intro/03_palindromes.py
@@ -1,8 +1,22 @@
+import re
+
+
 def is_palindrome(string):
     # backwards = string[::-1]
     # return backwards == string
 
-    return string[::-1] == string
+    return string[::-1].casefold() == string.casefold()
 
-print(is_palindrome("lol"))
-print(is_palindrome("word"))
+
+print(is_palindrome("Lol"))
+print(is_palindrome("RaDar"))
+
+
+def palindrome_sentence(string):
+    cleaned_sentence = re.sub('[^a-zA-Z0-9]', '', string.lower())
+    return cleaned_sentence == cleaned_sentence[::-1]
+
+
+print(palindrome_sentence("A man, a plan, a canal, Panama!"))  # True
+print(palindrome_sentence("Madam, in Eden, I'm Adam."))  # True
+print(palindrome_sentence("Hello, world!"))  # False
