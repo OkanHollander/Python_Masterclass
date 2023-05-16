dabe090d207637cde6f1fcf1ad6b41022589694c
Okan Hollander <okan.hollander@quality.nl>
2023-05-16 15:21:09 +0200

    Initial Commit

---
+++ 05_Lists_and_Tuples/challenge01.py
@@ -0,0 +1,33 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+# write your code here
+for entry in data:
+    if 'flower' in entry.lower():
+        flowers.append(entry.replace(' - Flower', ''))
+    else:
+        shrubs.append(entry.replace(' - Shrub', ''))
+
+print(flowers)
+print(shrubs)
