ce2420424743b49a574d3a76b737fa8b358b331d
Okan Hollander <okan.hollander@quality.nl>
2023-05-16 16:42:14 +0200

    Safely removing values from a list

--- 05_Lists_and_Tuples/15_deleting_items_from_a_list.py
+++ 05_Lists_and_Tuples/15_deleting_items_from_a_list.py
@@ -4,7 +4,17 @@ data = [4, 5, 104, 105, 110, 120, 130, 150,
 min_valid = 100
 max_valid = 200
 
+# for index, value in enumerate(data):
+#     if (value < min_valid) or (value > max_valid):
+#         del data[index]
+# print(data)
+
+# process the low values in the list
+stop = 0
 for index, value in enumerate(data):
-    if (value < min_valid) or (value > max_valid):
-        del data[index]
+    if value >= min_valid:
+        stop = index
+        break
+print(stop)
+del data[:stop]
 print(data)
