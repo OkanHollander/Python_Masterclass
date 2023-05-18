8329e842b6c5123eb78ff9097c204dce8c54e61f
Okan Hollander <okan.hollander@quality.nl>
2023-05-18 18:52:11 +0200

    Initial Commit

---
+++ 10_Object_Oriented_Programming/01_oop.py
@@ -0,0 +1,19 @@
+class Kettle(object):
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+kenwood = Kettle('Kenwood', 1000)
+print(kenwood.make)
+print(kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle('Hamilton', 2000)
+print(hamilton.make)
+print(hamilton.price)
+
+print(f"Models: {kenwood.make} = {kenwood.price}, {hamilton.make} = {hamilton.price}")
