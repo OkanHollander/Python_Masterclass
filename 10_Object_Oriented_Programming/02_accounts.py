0cb7e0ace6033e798f57e467c35ec8e2f9119749
Okan Hollander <okan.hollander@quality.nl>
2023-05-19 07:25:11 +0200

    non public and mangling

--- 10_Object_Oriented_Programming/02_accounts.py
+++ 10_Object_Oriented_Programming/02_accounts.py
@@ -1,6 +1,7 @@
 import datetime
 import pytz
 
+
 class Account:
     """Simple account class with balance"""
 
@@ -10,35 +11,38 @@ class Account:
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance) -> None:
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print(f"Account created for {self.name}")
+        self._name = name
+        self._balance = balance
+        self._transaction_list = []
+        print(f"Account created for {self._name} with balance {self._balance}")
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
-            self.transaction_list.append((Account._current_time(), amount))
-            print(f"{self.name} deposited {amount} into account")
+            self._balance += amount
+            self._transaction_list.append((Account._current_time(), amount))
+            print(f"{self._name} deposited {amount} into account")
 
     def withdraw(self, amount):
-        if amount > 0 and amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account._current_time(), -amount))
-            print(f"{self.name} withdrew {amount}")
+        if amount > 0 and amount <= self._balance:
+            self._balance -= amount
+            self._transaction_list.append((Account._current_time(), - amount))
+            print(f"{self._name} withdrew {amount}")
         else:
             print("Insufficient funds")
 
     def show_balance(self):
-        print(f"{self.name}'s balance is {self.balance}")
-    
+        print(f"{self._name}'s balance is {self._balance}")
+
     def show_transactions(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 trans_type = "deposit"
             else:
                 trans_type = "withdrawal"
-            print(f"{trans_type} of {amount} on {date.astimezone(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')}")
+            print(
+                f"{trans_type} of {amount} on {date.astimezone(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')}"
+            )
+
 
 if __name__ == "__main__":
     account = Account("John", 1000)
@@ -46,3 +50,9 @@ if __name__ == "__main__":
     account.withdraw(500)
     account.show_balance()
     account.show_transactions()
+
+    steph = Account("Steph", 1000)
+    steph.deposit(500)
+    steph.withdraw(200)
+    steph.show_transactions()
+    steph.show_balance()
