afd5943d952da570d36cb1db6cb2978470fae8bc
Okan Hollander <okan.hollander@quality.nl>
2023-05-19 11:48:38 +0200

    Getters and Setters

---
+++ 10_Object_Oriented_Programming/main_04.py
@@ -0,0 +1,17 @@
+from player_04 import Player
+
+player_01 = Player('Okan')
+
+print(player_01.name)
+print(player_01.lives)
+player_01.lives -= 1
+print(player_01)
+
+player_01.lives -= 1
+print(player_01)
+
+player_01.lives -= 1
+print(player_01)
+
+player_01.lives -= 1
+print(player_01)
---
+++ 10_Object_Oriented_Programming/player_04.py
@@ -0,0 +1,21 @@
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self._lives = 3
+        self.level = 1
+        self.score = 0
+
+    def _get_lives(self):
+        return self._lives
+
+    def _set_lives(self, lives):
+        if lives >= 0:
+            self._lives = lives
+        else:
+            print("You can't have a negative lives")
+            self._lives = 0
+
+    lives = property(_get_lives, _set_lives)
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}, Lives: {self.lives}, Level {self.level}, Score: {self.score}"
